9756251d792d498bf659d144a967f400d339b0f2
filberttansil <filberttansil1212@gmail.com>
2023-08-24 22:16:17 +0700

    Feat: HomeScreen and Card for Packages

---
+++ Serenify-Mobile/components/CardCarousel.js
@@ -0,0 +1,86 @@
+import { View, Text, TouchableWithoutFeedback, Dimensions } from "react-native";
+import React from "react";
+import Carousel from "react-native-snap-carousel";
+import { useNavigation } from "@react-navigation/native";
+var { width, height } = Dimensions.get("window");
+export default function CardCarousel({ data }) {
+  const navigation = useNavigation();
+  const handleClick = () => {
+    // navigation.navigate("Movie", item);
+    navigation.navigate("SignUp");
+  };
+  return (
+    <View className="mb-8">
+      <Text className="text-[#1A1B4B] text-xl mx-4 mb-10 font-bold">
+        Available Packages
+      </Text>
+      <View className="mb-8 ">
+        <Carousel
+          data={data}
+          renderItem={({ item }) => (
+            <Package handleClick={handleClick} item={item} />
+          )}
+          firstItem={1}
+          // loop={true}
+          // inactiveSlideScale={0.86}
+          inactiveSlideOpacity={0.6}
+          sliderWidth={width}
+          itemWidth={width * 0.72}
+          slideStyle={{
+            display: "flex",
+            alignItems: "center",
+            shadowColor: "gray",
+            shadowRadius: 5,
+            shadowOffset: { width: 5, height: 5 },
+            shadowOpacity: 1,
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const Package = ({ item, handleClick }) => {
+  return (
+    // Jika di tekan akan langsung ke card utk bayar
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+      <View className="bg-neutral-200 w-full h-[400px] rounded-xl my-2">
+        <View className="flex-row justify-between items-center mx-4 mt-8 border-b-2 border-neutral-400 pb-2">
+          <View className="flex-1">
+            <Text className="text-black text-lg font-semibold">PAKET</Text>
+            <Text className="text-black text-xl font-semibold">
+              Small {/* {package.name} */}
+            </Text>
+          </View>
+
+          <Text className="text-[#3E6D9C] text-2xl font-bold">
+            Rp.299k {/* {package.price} */}
+          </Text>
+        </View>
+        <View className="flex-row my-4 justify-between mx-24">
+          <Text className="text-center text-xl font-semibold ">SESI</Text>
+          <Text className="text-center text-xl font-semibold ">
+            1 JAM {/* {package.duration} */}
+          </Text>
+        </View>
+
+        <View className="mx-4 border-t-2 border-neutral-400 py-4">
+          <Text className="mb-2 font-semibold">Description</Text>
+          <Text className="tracking-wide text-base text-neutral-800">
+            Paket yang cocok bila kamu lagi ingin irit budget, paket ini akan
+            berlangsung selama 2 jam. Saya gak tau bang , saya cuman kerja bang.
+            {/* package.description */}
+          </Text>
+        </View>
+        {/* Schedule */}
+        <View className="my-2 mx-4">
+          <Text className="mb-2 font-semibold">Schedule</Text>
+          <Text className="tracking-wide text-base text-neutral-800">
+            Monday, 20 October 2023
+            {/* package.schedule */}
+          </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
--- Serenify-Mobile/screens/HomeScreen.js
+++ Serenify-Mobile/screens/HomeScreen.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { themeColors } from "../themes";
+import CardCarousel from "../components/CardCarousel";
 
 export default function HomeScreen() {
+  // Dummy (replace dengan fetch API)
+  const [packages, setPackages] = useState([1, 1, 1, 1, 1]);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1" style={{ backgroundColor: themeColors.bg }}>
+      <SafeAreaView>
+        <View className="flex-row justify-between items-center mx-4">
+          <Image
+            source={require("../assets/SerenifyLogo.png")}
+            style={{ width: 120, height: 60 }}
+          />
+          <Text className="text-[#1A1B4B] mr-4  text-base">Hello, Putri</Text>
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      >
+        <CardCarousel data={packages} />
+      </ScrollView>
     </View>
   );
 }
