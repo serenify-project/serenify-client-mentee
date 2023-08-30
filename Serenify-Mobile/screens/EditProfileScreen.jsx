21dcb38d254faaf5dd204df274401585e7463108
putriindah <49981319+putriindaht@users.noreply.github.com>
2023-08-30 16:07:32 +0700

    Update EditProfileScreen.jsx

--- Serenify-Mobile/screens/EditProfileScreen.jsx
+++ Serenify-Mobile/screens/EditProfileScreen.jsx
@@ -1,50 +1,44 @@
-import { View, Text } from "react-native";
 import React, { useState } from "react";
 import Form from "../components/Form";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  editUser,
-  getUserById,
-} from "../stores/actions/actionCreators.js/user";
-import { useNavigation } from "@react-navigation/native";
-export default function EditProfileScreen() {
-  const { user } = useSelector((state) => state.user);
-  const navigation = useNavigation();
-  const [form, setForm] = useState(
-    user
-      ? {
-          username: user.username,
-          email: user.email,
-          gender: user.gender,
-          birthDate: user.birthDate,
-        }
-      : { username: "", email: "", password: "", gender: "", birthDate: "" }
-  );
-  const dispatch = useDispatch();
-  const handleChange = (field, value) => {
-    setForm((prevUser) => ({
-      ...prevUser,
-      [field]: value,
-    }));
-  };
-  // Pemanggilan API (trycatch)
-  const handleSubmit = async (event) => {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_URL } from "../config/api";
+export default function EditProfileScreen({ route, navigation }) {
+  const populatedData = route?.params
+  const [form, setForm] = useState(populatedData);
+
+  const handleSubmit = async () => {
     try {
-      await dispatch(editUser(form, user.id));
-      // setelah submit fetch ulang user berdasarkan id
-      await dispatch(getUserById(user.id));
-      navigation.navigate("Profile");
+      const token = await AsyncStorage.getItem('access_token')
+      console.log(token)
+      const response = await fetch(`${API_URL}/users/edit`, {
+        method: 'put',
+        headers: {
+          "Content-Type": "application/json",
+          access_token: token
+        },
+        body: JSON.stringify({
+          username: form.username,
+          gender: form.gender,
+          birthDate: form.birthDate,
+        })
+      })
+      if (response.ok) {
+        navigation.navigate("Profile");
+      } else {
+        throw Error('response not ok')
+      }
     } catch (err) {
       console.log(err);
     }
   };
   return (
     <Form
-    title={"Edit Profile"}
+      title={"Edit Profile"}
       data={form}
       isEditPage={true}
-      handleChange={handleChange}
+      handleChange={setForm}
       handleSubmit={handleSubmit}
+      isEdit
     />
   );
 }
